cb9cb7df48b2806dfc86dafa3b4de981019a5f19
Miguel Costa <miguelpintodacosta@gmail.com>
2018-01-24 22:21:15 +0000

    Add project

---
+++ src/Mpc.DotNetCoreService.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mpc.DotNetCoreService.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
